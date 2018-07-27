35c0799438ae70506a1653c6f5753526f5d7e498
surendravarma1557 <41769678+surendravarma1557@users.noreply.github.com>
2018-07-27 16:04:38 +0530

    Update prob5.py

--- prob5.py
+++ prob5.py
@@ -1,9 +1,9 @@
 n1=raw_input()
 n2=raw_input()
 n3=raw_input()
-if(n1 > n2 and n1 > n3):
+if(n1 >= n2 and n1 >= n3):
 	print('n1')
-elif(n1 < n2 and n2 > n3):
+elif(n1 <= n2 and n2 >= n3):
 	print('n2')
 else:
 	print('n3')
