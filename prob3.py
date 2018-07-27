e2970223b42c81cb880d65fe30d88aadeeb02eab
surendravarma1557 <41769678+surendravarma1557@users.noreply.github.com>
2018-07-27 15:13:59 +0530

    Create prob3.py

---
+++ prob3.py
@@ -0,0 +1,9 @@
+# varma3
+n=raw_input()
+if(n >= 'a'and n <='z') or (n >= 'A' and n <='Z'):
+	if n in['a','e','i','o','u','A','E','I','O','U']:
+		print('Vowel')
+	else:
+		print('Consonant')
+else:
+	print('Invalid')
